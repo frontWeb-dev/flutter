007d165cd7e0acac07440c49fe1f8dbaf29ec8b5
frontweb-dev <frontend_web@kakao.com>
2023-05-09 13:00:06 +0900

    feat #3.0 ~ 3.4
    - header
    - buttons section
    - code actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,109 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 // root
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Theme setting : Material(goggle) || Cupertino(ios);
+    var elevatedButton = ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+        backgroundColor: const Color(0xff181818),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+        ),
+      ),
+      onPressed: () {},
+      child: const Text('Request',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300)),
+    );
+
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.red.shade200,
-          body: const Center(
-            child: Text("hello flutter"),
+          backgroundColor: const Color(0xFF181818),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          'Hey,Selena',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 34,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                      fontSize: 22, color: Colors.white.withOpacity(0.8)),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  '\$5,194,482',
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 160,
+                      child: elevatedButton,
+                    )
+                  ],
+                ),
+              ],
+            ),
           )),
     );
   }
