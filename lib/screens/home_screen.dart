77fda4b71829bea6fbab62fc1fe00e0451ef116d
frontweb-dev <frontend_web@kakao.com>
2023-05-11 11:07:10 +0900

    feat: data format
    - data reset

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,14 +9,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int totalSecond = 1500; // 25min
+  static const twentyFiveMin = 1500;
+  int totalSecond = twentyFiveMin; // 25min
+  int totalPomodoros = 0;
   bool isRunning = false;
+
   late Timer timer;
 
   void onTick(Timer timer) {
-    setState(() {
-      totalSecond = totalSecond - 1;
-    });
+    // reset
+    if (totalSecond == 0) {
+      setState(() {
+        totalPomodoros = totalPomodoros + 1;
+        isRunning = false;
+        totalSecond = twentyFiveMin;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSecond = totalSecond - 1;
+      });
+    }
   }
 
   void onStartPressed() {
@@ -33,6 +46,11 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds); // 0:00:00.00000
+    return (duration.toString().split(".").first.substring(2)); // 00:00
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                "$totalSecond",
+                format(totalSecond),
                 style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 90,
@@ -94,7 +112,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                           ),
                           Text(
-                            "0",
+                            "$totalPomodoros",
                             style: TextStyle(
                               fontSize: 60,
                               color: Theme.of(context)
