be47d78e9c10ea9878fa1e730019e878e39574c0
frontweb-dev <frontend_web@kakao.com>
2023-05-12 15:34:32 +0900

    issue: image 받아오지 못하는 이슈 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,30 @@
+import 'dart:io';
+
+import 'package:coin_flt/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-import 'screens/home_screen.dart';
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+  }
+}
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const App());
 }
 
-class App extends StatefulWidget {
-  const App({super.key});
+class App extends StatelessWidget {
+  const App({super.key}); // 식별자
 
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+      theme: ThemeData(fontFamily: 'SCDream'),
+      home: HomeScreen(),
     );
   }
 }
