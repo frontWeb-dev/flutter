6afc49fc60ecfd3e4448bf65a2736f7f7e119435
frontweb-dev <frontend_web@kakao.com>
2023-05-12 16:23:17 +0900

    feat: 상세 정보 데이터 fetching

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'package:coin_flt/models/webtoon_detail_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:coin_flt/models/webtoon_model.dart';
+import 'package:coin_flt/models/webtoon_episode_model.dart';
 
 class ApiService {
   static const String baseUrl =
@@ -23,6 +25,35 @@ class ApiService {
         webtoonInstances.add(instance);
       }
       return webtoonInstances;
+    }
+
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLastestEpisodesByid(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
 
     throw Error();
