a9b6f0097b2379014e0833d059c3935e4ec99c7a
frontweb-dev <frontend_web@kakao.com>
2023-06-17 18:52:49 +0900

    feat: 초기화

--- android/app/src/main/kotlin/com/example/coin_flt/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coin_flt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/webtoon_episode_model.dart
+++
@@ -1,9 +0,0 @@
-class WebtoonEpisodeModel {
-  final String title, rating, id, date;
-
-  WebtoonEpisodeModel.fromJson(Map<String, dynamic> json)
-      : title = json["title"],
-        rating = json["rating"],
-        id = json["id"],
-        date = json["date"];
-}
