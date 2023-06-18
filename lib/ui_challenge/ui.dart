7feef381a69676505f57c1f05ed37b8798ffb314
frontweb-dev <frontend_web@kakao.com>
2023-06-18 17:34:09 +0900

    feat: UI CHALLENGE _ Button Section

--- lib/ui_challenge/ui.dart
+++ lib/ui_challenge/ui.dart
@@ -8,6 +8,7 @@ class UI extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(
             height: 80,
@@ -32,6 +33,64 @@ class UI extends StatelessWidget {
                     style: TextStyle(
                       color: Colors.white.withOpacity(0.8),
                       fontSize: 18,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 120),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text(
+                '\$5 194 382',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 44,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFF2B33A),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(fontSize: 20, letterSpacing: -0.5),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF181818),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                      child: Text(
+                        'Request',
+                        style: TextStyle(
+                            fontSize: 20,
+                            letterSpacing: -0.5,
+                            color: Colors.white),
+                      ),
                     ),
                   ),
                 ],
