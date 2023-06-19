4f50f362077f96c39284bb2ccefbc359a882a960
frontWeb-dev <101559564+frontWeb-dev@users.noreply.github.com>
2023-06-19 11:08:54 +0900

    feat: Reusable Widgets

--- lib/ui_challenge/ui.dart
+++ lib/ui_challenge/ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './widgets/button.dart';
 
 class UI extends StatelessWidget {
   const UI({super.key});
@@ -59,39 +60,18 @@ class UI extends StatelessWidget {
                     fontWeight: FontWeight.w600),
               ),
               const SizedBox(height: 20),
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFF2B33A),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                      child: Text(
-                        'Transfer',
-                        style: TextStyle(fontSize: 20, letterSpacing: -0.5),
-                      ),
-                    ),
+                  Button(
+                    text: 'Transfer',
+                    bgColor: Color(0xFFF2B33A),
+                    textColor: Colors.black,
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF181818),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                      child: Text(
-                        'Request',
-                        style: TextStyle(
-                            fontSize: 20,
-                            letterSpacing: -0.5,
-                            color: Colors.white),
-                      ),
-                    ),
+                  Button(
+                    text: 'Request',
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white,
                   ),
                 ],
               )
